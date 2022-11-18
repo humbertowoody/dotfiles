6ca0e52c4f470056d23231912fdaa66d47966c46
Humberto Alcocer <humbertowoody@gmail.com>
2022-11-17 20:48:14 -0600

    wakeup: add wakeup-list function to list defined MAC addresses variables

--- scripts/tools/wakeup.sh
+++ scripts/tools/wakeup.sh
@@ -6,6 +6,7 @@
 # MAC Addresses constants.
 MAC_ADDRESS_DELL_WINDOWS=84:2b:2b:a2:1c:f7
 MAC_ADDRESS_ALCOCERSERVER=00:21:9b:0c:21:5d
+MAC_ADDRESS_SYNOLOGY_NAS=00:11:32:fa:bb:be
 
 # Function definition
 function wakeup()
@@ -14,10 +15,20 @@ function wakeup()
   wakeonlan $deviceMacAddress
 }
 
+# Function to print the MAC addresses available
+function wakeup-list()
+{
+  # Iterate through all variables matching regex.
+  for mac in `typeset +m 'MAC_ADDRESS_*'`;
+  do
+    # Print each MAC address.
+    echo "$mac=${(P)mac}"
+  done
+}
+
 # I own a Synology NAS
 # To use the wakeup function from within the DSM, the command would
 # be:
 #   sudo synonet --wake [MAC Address] [interface]
 # example:
 #   sudo synonet --wake 84:2b:2b:a2:1c:f7 bond0
-
