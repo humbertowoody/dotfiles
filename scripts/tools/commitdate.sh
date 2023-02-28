ea4160434dff6b28c83041b906abdbc97c153507
Humberto Alcocer <humbertowoody@gmail.com>
2023-02-28 15:59:31 -0600

    feat(tools): add a new script to commit current date

---
+++ scripts/tools/commitdate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# commitdate - @humbertowoody
+# Creates a new commit given the current date for my school 
+# repositories that get updated with each class.
+
+
+function commitdate()
+{
+  git commit -m "Clase del `date +%d-%m-%Y` 🎉";
+}
